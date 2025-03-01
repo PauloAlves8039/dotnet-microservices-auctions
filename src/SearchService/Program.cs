f70ee5b35017b762e76e2d67ed8a59a8d6aa139d
PauloAlves8039 <pj38alves@gmail.com>
2025-03-01 12:34:51 -0300

    feat: create model class and mongodb configurations

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,8 +1,19 @@
+using SearchService.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
+
+try
+{
+    await DbInitializer.InitDb(app);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
 
 app.UseAuthorization();
 
