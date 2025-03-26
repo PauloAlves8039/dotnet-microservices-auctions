1bf71de6fb090e112c6fdb8713809a860dcc6ac4
PauloAlves8039 <pj38alves@gmail.com>
2025-03-26 11:15:29 -0300

    feat: using zustand for state management

--- frontend/carsties/app/auctions/Filters.tsx
+++ frontend/carsties/app/auctions/Filters.tsx
@@ -1,6 +1,7 @@
 
 import { Button, ButtonGroup } from "flowbite-react";
 import React from "react";
+import { useParamsStore } from "../hooks/useParamsStore";
 
 type Props = {
     pageSize: number;
@@ -9,24 +10,27 @@ type Props = {
 
 const pageSizeButtons = [4, 8, 12];
 
-export default function Filters({pageSize, setPageSize}: Props) {
-  return (
-      <div className="flex justify-between items-center mb-4">
-          <div>
-              <span className="uppercase text-sm text-gray-500 mr-2">Page size</span>
-              <ButtonGroup>
+export default function Filters() {
+    const pageSize = useParamsStore(state => state.pageSize);
+    const setParams = useParamsStore(state => state.setParams);
+
+    return (
+        <div className="flex justify-between items-center mb-4">
+            <div>
+                <span className="uppercase text-sm text-gray-500 mr-2">Page size</span>
+                <Button.Group>
                 {pageSizeButtons.map((value, i) => (
                     <Button 
                         key={i} 
-                        onClick={() => setPageSize(value)}
+                        onClick={() => setParams({pageSize: value})}
                         color={`${pageSize === value ? "red" : "gray"}`}
                         className="focus:ring-0"
                     >
                         {value}
                     </Button>
                 ))}
-              </ButtonGroup>
-          </div>
-      </div>
+                </Button.Group>
+            </div>
+        </div>
   )
 }
